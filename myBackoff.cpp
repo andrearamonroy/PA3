033ed66e05a13338616900c816e2d9e8ad0e3b3a
dannyd2 <dannydfpv@gmail.com>
2018-11-29 16:01:01 -0600

    Add files via upload

--- myBackoff.cpp
+++ myBackoff.cpp
@@ -1,35 +1,47 @@
 #include <iostream>
 #include <fstream>
-
+#include <math.h>
+#include <bits/stdc++.h> 
 using namespace std;
 
 int main() {
 	
-	int devices = 100;
-	
 	ofstream linearLatency;
 	linearLatency.open("linearLatency.txt");
-	
-	for(int number_of_devices = 100; number_of_devices < 6001; number_of_devices += 100) {
-		for(int trial = 0; trial < 10; trial++) {
+	for(int number_of_devices = 100; number_of_devices < 6001; number_of_devices += 100) {	
+		for(int trial = 0; trial < 1; trial++) {
+
+			int devices = number_of_devices;
+			int windows_size=2;
 			
-			//Implement Linear Backoff Here
-			
-			
-			
-			
-			
-			
-			
-			
-			
+			while (devices > 0) {
+				int Device_Choices[windows_size];
+				for(int i = 0; i < windows_size; i++){
+					Device_Choices[i] = 0;
+				}
+				for(int i = 0; i < number_of_devices; i++){
+					int random_var = rand()%windows_size;
+					Device_Choices[random_var] = Device_Choices[random_var]+1;
+				}
+				for (int i = 0; i < windows_size; i++){
+					if (Device_Choices[i]==1){
+						devices--;
+					}
+				
+				}
+				windows_size++;
+
+			}
 			
 		}
+		
 	}
+	
+
 	linearLatency.close();
 	
 	
-	
+	/*
 	devices = 100;
 	
 	ofstream binaryLatency;
@@ -87,6 +99,7 @@ int main() {
 		}
 	}
 	logLatency.close();
-	
+	*/
+	return 0;
 	
 }
